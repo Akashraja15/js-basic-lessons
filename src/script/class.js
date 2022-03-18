297d6696e912d7a781e2ddfa19fcc6f86a73550f
Akash Raja D <akas2989@global.chainsys.com>
2022-03-18 14:51:27 +0530

    including files

---
+++ src/script/class.js
@@ -0,0 +1,66 @@
+/* eslint-disable semi */
+
+class Pizza {
+  // Contructor
+  constructor (toppings = [], customer) {
+    this.toppings = toppings;
+    this.customer = customer;
+  }
+  //Static property
+  static extraToppings = ['pepperoni', 'Cheese'];
+  //static method
+  static sayThankyou() {
+      console.log('Thank you!!!');
+  }
+  // instance property
+  slices = 10;
+
+  get noOfSlices() {
+      return this.slices;
+  }
+//   set noOfSlices(value) {
+//       slices = value;
+//       this.slices = values;
+//   }
+
+  eat () {
+    console.log(this.toppings);
+    console.log(this.customer);
+  }
+  // instance method
+  getCokeOrPepsi = () => {
+      console.log('Coke');
+      console.log('Pepsi');
+      console.log(this);
+  }
+  // Private field can only be modified inside a class
+  #bankBalance = 10000;
+
+  get balance() {
+      return this.#bankBalancebankBalance;
+  }
+  set balance(value) {
+      this.#bankBalancebankBalance = value;
+  }
+
+}
+const myLunch = new Pizza(['onion', 'mushroom', 'capsicum'], 'Akash');
+// console.log(myLunch);
+myLunch.eat();
+myLunch.toppings = ['Carrot', 'Almonds', 'Lobster']
+console.log(myLunch.toppings);
+console.log(Pizza.extraToppings);
+Pizza.extraToppings = ['onions', 'Lettuce'];
+console.log(Pizza.extraToppings);
+Pizza.sayThankyou();
+console.log(myLunch.noOfSlices);
+myLunch.noOfSlices = 6;
+console.log(myLunch.noOfSlices);
+myLunch.getCokeOrPepsi();
+myLunch.slices = 12;
+console.log(myLunch.noOfSlices);
+console.log(myLunch.balance);
+myLunch.balance = 5000;
+console.log(myLunch.balance);
+myLunch.#bankBalance = 2000;
+console.log(myLunch.balance);
--- src/script/task.js
+++ src/script/task.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable semi */
 function doSomeTask () {
   const x = 100;
@@ -13,9 +14,47 @@ function outer () {
   function inner () {
     const innerVar = 'Hey I am an inner var!';
     console.log(innerVar);
-    console.log(outerVar);
+    console.log(outerVar); // Closure - captured variable
   }
   // console.log(innerVar); // Uncaught referenceError: innerVar is not defined
+  //   inner();
   return inner;
 }
-outer();
+// outer();
+// outer.inner();
+const innerFunc = outer();
+innerFunc();
+
+// --------------------------------------------------------------------------------------------------
+function createGreeting (greeting = '') {
+  const myGreet = greeting.toUpperCase();
+  return function (name) {
+    return `${myGreet} ${name}`;
+  }
+}
+const greet = createGreeting('Welcome!');
+console.log(greet('Akash'));
+
+const sayHello = createGreeting('Hello');
+const sayVanakkam = createGreeting('Vanakkam');
+console.log(sayHello('Akash Raja'));
+console.log(sayVanakkam('Daniel'));
+console.log(sayVanakkam('Raj'));
+
+// ----------------------------------------------------------------------------------------
+
+function createGame (gameName) {
+  let score = 0;
+  return function win () {
+    score++;
+    return `Your name ${gameName} score is ${score}`;
+  }
+}
+// console.log(createGame('Akash Raja')());
+
+const hockeyGame = createGame('Hockey');
+const soccerGame = createGame('Soccer');
+hockeyGame();
+hockeyGame();
+hockeyGame();
+soccerGame();
