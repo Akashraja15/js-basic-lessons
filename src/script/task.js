317c9180e5d7b95721fbe1161d80bc1a20e9f533
Akash Raja D <akas2989@global.chainsys.com>
2022-03-18 10:34:20 +0530

    including files

--- src/script/task.js
+++ src/script/task.js
@@ -2,6 +2,20 @@
 function doSomeTask () {
   const x = 100;
   const y = 20;
-  console.log('x: ' + x + 'y: ' + y);
+  console.log('x: ' + x + ' y: ' + y);
 }
 doSomeTask();
+console.log('completed');
+
+function outer () {
+  const outerVar = 'Hey I am the outer Var!';
+  console.log(outerVar);
+  function inner () {
+    const innerVar = 'Hey I am an inner var!';
+    console.log(innerVar);
+    console.log(outerVar);
+  }
+  // console.log(innerVar); // Uncaught referenceError: innerVar is not defined
+  return inner;
+}
+outer();
