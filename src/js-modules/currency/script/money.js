a3e50cf2abd7e8988378794c36d11b3cac7750eb
Akash Raja D <akas2989@global.chainsys.com>
2022-03-28 09:44:37 +0530

    including files

--- src/js-modules/currency/script/money.js
+++ src/js-modules/currency/script/money.js
@@ -13,7 +13,7 @@ function getOptions(data) {
     }
     document.getElementById("amount").addEventListener("change", myFunction);
  export function myFunction() {
-var currency = document.getElementById("amount");
+var currency = document.getElementById("amount").value;
 var result = currency * 4;
 document.getElementById('result').innerHTML=result;
 }
